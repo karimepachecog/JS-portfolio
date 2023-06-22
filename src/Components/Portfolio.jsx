9edc2a68c179095e2c422e4202a3a59ff52b9a04
akkarie <a22216265@alumnos.uady.mx>
2023-06-22 00:51:50 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/Mi proyecto (1).jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,23 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Taller Mifi-Cloud",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Collaboration with Jonathan Castillo and Felipe Flores.",
+    url: "https://github.com/karimepachecog/taller-mifi-cloud",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Taller Bases de datos",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Material para Bases de datos con Kutso.",
+    url: "https://github.com/karimepachecog/Bases-de-datos",
   },
+
   {
-    title: "My Resume Site",
+    title: "MLSA Application",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Video  de aplicación al programa.",
+    url: "https://youtu.be/UhAUn22o0hk",
   },
 ];
 
